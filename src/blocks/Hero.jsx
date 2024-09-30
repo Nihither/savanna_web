8eddce3a7de4d38d33368bba906b10aa95795097
nihither <semenov.sh.an@gmail.com>
2024-09-30 23:29:35 +0300

    add hero template

---
+++ src/blocks/Hero.jsx
@@ -0,0 +1,14 @@
+
+
+
+function Hero() {
+  return (
+    <div className="container-fluid bg-info" style={{ height: "50vh" }}>
+      <div className="container">
+        <h1>Английский начиванется здесь</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
