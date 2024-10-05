76339e6e71b0958366ba03a09bb00ee07c321738
nihither <semenov.sh.an@gmail.com>
2024-10-06 01:28:01 +0300

    correct grid

--- src/blocks/CertBanner.jsx
+++ src/blocks/CertBanner.jsx
@@ -3,24 +3,26 @@ import cert_image from '../assets/certificate.jpg'
 
 function CertBanner() {
   return (
-    <div className="container py-5">
-      <div className="d-flex flex-column">
-        <div className="row">
-          <div className="col-md-3"></div>
-          <div className="col-md-6">
-            <img src={cert_image} alt="cert_image" className="card-img"/>
+    <div className="container-fluid bg-light">
+      <div className="container py-5">
+        <div className="d-flex flex-column">
+          <div className="row">
+            <div className="col-md-3"></div>
+            <div className="col-md-6">
+              <img src={cert_image} alt="cert_image" className="card-img"/>
+            </div>
+            <div className="col-md-3"></div>
           </div>
-          <div className="col-md-3"></div>
-        </div>
-        <div className="vstack gap-4 justify-content-center align-items-center">
-          <h1 className="display-4 text-center pt-5">Дарите знания</h1>
-          <div className="">
-            <p className="text-center">С недавних пор в Savanna ABC появились сертификаты на обучение.</p>
-            <p className="text-center">Теперь вы можете подарить вашим близким по-настоящему бесценный подарок —
-              знания.</p>
-            <small className="text-center">Сертификат можно приобрести на любую сумму.</small>
+          <div className="vstack gap-4 justify-content-center align-items-center">
+            <h1 className="display-4 text-center pt-5">Дарите знания</h1>
+            <div className="">
+              <p className="text-center">С недавних пор в Savanna ABC появились сертификаты на обучение.</p>
+              <p className="text-center">Теперь вы можете подарить вашим близким по-настоящему бесценный подарок —
+                знания.</p>
+              <p className="text-center"><small>Сертификат можно приобрести на любую сумму.</small></p>
+            </div>
+            <button className="btn btn-info">Приобрести сертификат</button>
           </div>
-          <button className="btn btn-info">Приобрести сертификат</button>
         </div>
       </div>
     </div>
--- src/blocks/Footer.jsx
+++ src/blocks/Footer.jsx
@@ -4,10 +4,12 @@
 function Footer() {
   return (
     <footer className="footer">
-      <div className="container py-3 border-top">
-        <div className="row">
-          <div className="col-12">
-            <p className="text-center">Savanna ABC</p>
+      <div className="container-fluid bg-light border-top">
+        <div className="container py-3">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center m-0">Savanna ABC</p>
+            </div>
           </div>
         </div>
       </div>
