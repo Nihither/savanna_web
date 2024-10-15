1696ffb10456b7eeef0c43dcaf18230b9722df9a
nihither <semenov.sh.an@gmail.com>
2024-10-15 11:00:42 +0300

    change bg at hero block

--- src/blocks/Hero.jsx
+++ src/blocks/Hero.jsx
@@ -1,11 +1,16 @@
-
+import hero_bg from '../assets/hero_bg.jpg'
 
 
 function Hero() {
   return (
-    <div className="hero container-fluid bg-info" style={{ height: "50vh" }}>
-      <div className="container">
-        <h1>Английский начиванется здесь</h1>
+    <div className="hero container-fluid p-0 position-relative">
+      <div className="container-fluid p-0">
+        <img src={hero_bg} alt="" className="bg-image d-lg-none position-absolute top-0 left-0" />
+      </div>
+      <div className="container-fluid position-relative z-2 bg-black bg-opacity-50">
+        <div className="content-block container d-flex align-items-center">
+          <h1 className="display-6 text-light text-center text-lg-start">Английский начинается здесь.</h1>
+        </div>
       </div>
     </div>
   );
