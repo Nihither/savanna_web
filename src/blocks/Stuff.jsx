0754e557800869955e09bfc5391530bafbbf9533
nihither <semenov.sh.an@gmail.com>
2024-10-18 17:46:12 +0300

    switch stuff block to stuff data

--- src/blocks/Stuff.jsx
+++ src/blocks/Stuff.jsx
@@ -1,23 +1,31 @@
 import StuffCard from "./elements/StuffCard.jsx";
+import PropTypes from "prop-types";
+import stuff_data from "../configs/stuff_data.json"
 
 
-function Stuff() {
+function Stuff(props) {
+
+  const stuff_ids = props.stuff_ids;
+  const stuff = stuff_ids.map((stuff_id) =>
+    <div key={stuff_id.toString()} className="col">
+      <StuffCard key={stuff_id.toString()} name={stuff_data.stuff[stuff_id].name} role={stuff_data.stuff[stuff_id].role}/>
+    </div>
+  )
+
   return (
     <div className="stuff container-fluid py-4 bg-light">
       <h1 className="text-center display-5 py-4">Наши преподаватели</h1>
       <div className="container">
         <div className="row row-cols-lg-4 row-cols-md-3 row-cols-1">
-          <div className="col"><StuffCard/></div>
-          <div className="col"><StuffCard/></div>
-          <div className="col"><StuffCard/></div>
-          <div className="col"><StuffCard/></div>
-          <div className="col"><StuffCard/></div>
-          <div className="col"><StuffCard/></div>
-          <div className="col"><StuffCard/></div>
+          {stuff}
         </div>
       </div>
     </div>
   );
+}
+
+Stuff.propTypes = {
+  stuff_ids: PropTypes.array,
 }
 
 export default Stuff;
--- src/blocks/elements/StuffCard.jsx
+++ src/blocks/elements/StuffCard.jsx
@@ -1,17 +1,23 @@
 import contact_us from '../../assets/contact_us.jpg'
+import PropTypes from "prop-types";
 
 
-function StuffCard() {
+function StuffCard(props) {
   return (
     <div className="card text-center border-0 d-flex align-items-center justify-content-center h-100
                     py-lg-5 py-4 bg-light">
       <img src={contact_us} alt="" className=" rounded-circle" width="90"/>
       <div className="card-body">
-        <h5 className="card-title">Савченко Анна</h5>
-        <p className="card-text text-secondary"><em>Автор проекта</em></p>
+        <h5 className="card-title">{props.name}</h5>
+        <p className="card-text text-secondary"><em>{props.role}</em></p>
       </div>
     </div>
   );
+}
+
+StuffCard.propTypes = {
+  name: PropTypes.string,
+  role: PropTypes.string,
 }
 
 export default StuffCard;
