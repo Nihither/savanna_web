57b0deb029eccf17c09cdfba3eaecf7558f64ff8
nihither <semenov.sh.an@gmail.com>
2024-10-20 09:27:06 +0300

    change stuff props

--- src/blocks/elements/StuffCard.jsx
+++ src/blocks/elements/StuffCard.jsx
@@ -6,7 +6,7 @@ function StuffCard(props) {
   return (
     <div className="card text-center border-0 d-flex align-items-center justify-content-center h-100
                     py-lg-5 py-4 bg-light">
-      <img src={"src/assets/stuff_avatars/"+props.avatar+".jpg"} alt="" className=" rounded-circle" width="90"/>
+      <img src={"src/assets/stuff_avatars/"+props.avatar} alt="" className=" rounded-circle" width="90"/>
       <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
         <p className="card-text text-secondary"><em>{props.role}</em></p>
