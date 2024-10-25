ef461dfccfbc0d2610a0b682eb7345bc6b31b93a
nihither <semenov.sh.an@gmail.com>
2024-10-26 00:51:42 +0300

    change padding

--- src/blocks/Offers.jsx
+++ src/blocks/Offers.jsx
@@ -10,8 +10,8 @@ function Offers(props) {
 
   return (
     <div className="offers">
-      <div className="container-fluid">
-        <div className="container my-5 text-center">
+      <div className="container-fluid bg-light">
+        <div className="container py-5 text-center">
           <div className="py-4"><h1 className="mb-4">Корпоративный английский</h1>
             <p className="fs-5 text-secondary">Обучайте Ваших сотрудников и их детей в Savanna ABC.</p>
             <p className="fs-5 text-secondary">Для корпоративных клиентов мы разработали:</p></div>
